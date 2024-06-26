084c9687d580c6572622ad75589efa0f2db21f94
skydreamer <skydreamer210@gmail.com>
2024-06-26 11:09:06 +0900

    chore: 프로젝트 초기세팅

---
+++ src/main/java/org/skydream/dev_step_be/DevStepBeApplication.java
@@ -0,0 +1,13 @@
+package org.skydream.dev_step_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevStepBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevStepBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skydream/dev_step_be/DevStepBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skydream.dev_step_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevStepBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
